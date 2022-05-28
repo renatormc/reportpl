8a119fa1d420f366976a563f7bfde98e1aef23c4
renatormc <renatomartinsrmc@gmail.com>
2022-05-28 09:50:06 -0300

    Normal

--- report_writer/doc_handler/subdoc_html/elment_parses/__init__.py
+++ report_writer/doc_handler/subdoc_html/elment_parses/__init__.py
@@ -2,18 +2,12 @@ from docxtpl.subdoc import Subdoc
 from .paragraph import parse_paragraph
 from .heading import parse_heading
 
-parsers = {
-    'p': parse_paragraph,
-    'h1': parse_heading,
-    'h2': parse_heading,
-    'h3': parse_heading,
-    'h4': parse_heading,
-    'h5': parse_heading
-}
 
 def parse_element(doc: Subdoc, el):
-    try:
-        parsers[el.name](doc, el)
-    except KeyError:
+    if el.name == "p":
+        parse_paragraph(doc, el)
+    elif el.name in ['h1', 'h2', 'h3', 'h4', 'h5']:
+        parse_heading(doc, el)
+    else:
         raise Exception(f"Element of type \"{el.name}\" not implemented")
     
--- report_writer/doc_handler/subdoc_html/elment_parses/heading.py
+++ report_writer/doc_handler/subdoc_html/elment_parses/heading.py
@@ -3,12 +3,12 @@ from .utils import remove_extra_spaces
 from bs4.element import Tag
 from .paragraph import parse_paragraph
 
-def parse_heading(sd: Subdoc, el: Tag):
+def parse_heading(sd: Subdoc, el: Tag):   
     level = int(el.name[1:])
     p = sd.add_paragraph(remove_extra_spaces(el.text))
     p.style = f"Título {level}"
     # print(level)
-    # sd.add_heading(remove_extra_spaces(el.text), level=level, style=f"Título {level}")
+    # sd.add_heading(remove_extra_spaces(el.text), level=level)
 
 
 
