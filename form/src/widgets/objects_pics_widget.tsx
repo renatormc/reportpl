1053a299b5075cf9957c23f845ad6e6205734ef3
renatormc <renatomartinsrmc@gmail.com>
2022-06-01 09:01:58 -0300

    Normal

--- form/src/widgets/objects_pics_widget.tsx
+++ form/src/widgets/objects_pics_widget.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Form, Image } from 'react-bootstrap';
 import { uploadWidgetAsset, urlForWidgetAsset } from '../services/api';
+import { SortableContainer , SortableElement } from "react-sortable-hoc";
 
 type Props = {
   model_name: string,
@@ -12,6 +13,22 @@ type Props = {
   randomID: string,
   updateFormValue: (field: string, value: any) => void
 }
+
+
+// const SortablePhoto = SortableElement(item => <Image
+//   key={index}
+//   className='ObjectsPicsImage'
+//   src={urlForWidgetAsset(props.randomID, props.field_name, item)} />);
+
+// const SortableGallery = SortableContainer(({ items }) => (
+//   <Gallery photos={items} renderImage={props => <SortablePhoto {...props} />} />
+// ));
+
+const SortableItem = SortableElement(({ value }: {value: string}) => <li>{value}</li>);
+
+const SortableList = SortableContainer(({children}: {children: Array<string>}) => {
+  return <ul>{children}</ul>;
+});
 
 function ObjectsPicsWidget(props: Props) {
   // const [selectedFiles, setSelectedFiles] = useState([])
@@ -28,6 +45,12 @@ function ObjectsPicsWidget(props: Props) {
     }
   }
 
+  const onSortEnd = ({ oldIndex, newIndex }: {oldIndex: number, newIndex: number}) => {
+    console.log(oldIndex, newIndex);
+    // setItems(arrayMove(items, oldIndex, newIndex));
+  };
+
+
   return (
     <div>
       <strong><Form.Label>{props.label}</Form.Label></strong>
@@ -35,15 +58,34 @@ function ObjectsPicsWidget(props: Props) {
         type="file"
         multiple
         onChange={uploadHandler} />
-      {props.data.not_classified.map((item: string, index: number) => {
+
+      {/* <SortableList items={props.data.not_classified} onSortEnd={onSortEnd} /> */}
+      <SortableList onSortEnd={onSortEnd}>
+        {props.data.not_classified.map((value: string, index: number) => (
+          <SortableItem key={index} index={index} value={urlForWidgetAsset(props.randomID, props.field_name, value)} />
+        ))}
+      </SortableList>
+
+      {/* {SortableContainer(({ items }) => {
         return (
-          <Image
-          key={index}
-          className='ObjectsPicsImage'
-            src={urlForWidgetAsset(props.randomID, props.field_name, item)}/>
-          )
-      })}
-            
+          props.data.not_classified.map((item: string, index: number) => {
+            return (
+              <SortableItem key={index} index={index} value={urlForWidgetAsset(props.randomID, props.field_name, item)} />
+              // <Image
+              //   key={index}
+              //   className='ObjectsPicsImage'
+              //   src={urlForWidgetAsset(props.randomID, props.field_name, item)} />
+            )
+          })
+        )
+      })} */}
+
+
+
+      {/* <SortableGallery items={props.data.not_classified} onSortEnd={onSortEnd} axis={"xy"} /> */}
+
+
+
       {props.errors && <div className="error-message">{props.errors}</div>}
     </div >
   );
