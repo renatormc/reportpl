253e797c581541f414403c9903be7e17b3e1c997
renatormc <renatomartinsrmc@gmail.com>
2022-06-08 23:47:30 -0300

    CPF Converter

--- report_writer/web_converters/__init__.py
+++ report_writer/web_converters/__init__.py
@@ -3,3 +3,4 @@ from .float_converter import FloatConverter
 from .int_converter import IntConverter
 from .path_converter import PathConverter
 from .string_list_converter import StringListConverter
+from .cpf_converter import CpfConverter
